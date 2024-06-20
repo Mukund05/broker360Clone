6876ee25a517af6c628360cd48d87c758a0984ca
abhishek <uxdesigns289@gmail.com>
2024-06-20 19:03:56 +0530

    working on image caorusel

--- src/components/PropertyDetails.jsx
+++ src/components/PropertyDetails.jsx
@@ -58,6 +58,7 @@ const PropertyDetails = () => {
       try {
         const response = await Api.getGallery(id);
         if (response.success) {
+          console.log(response.data);
           setImages(response.data);
         }
       } catch (error) {
@@ -246,6 +247,85 @@ const PropertyDetails = () => {
           <KeyboardBackspaceIcon />
           Atrás
         </Link>
+
+        {/* Caoursel Work Start */}
+
+        <div
+          id="default-carousel"
+          className="relative w-full"
+          data-carousel="slide"
+        >
+          <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+            {images.length > 0 &&
+              images.map((image) => (
+                <div
+                  key={image.id}
+                  className="hidden duration-700 ease-in-out"
+                  data-carousel-tem
+                >
+                  <img
+                    src={`${import.meta.env.VITE_BASE_URL}${
+                      image?.url || "dummy.jpg"
+                    }`}
+                    className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    alt="..."
+                  />
+                </div>
+              ))}
+          </div>
+
+          <button
+            type="button"
+            className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-prev
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg
+                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+              <span className="sr-only">Previous</span>
+            </span>
+          </button>
+          <button
+            type="button"
+            className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-next
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg
+                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span className="sr-only">Next</span>
+            </span>
+          </button>
+        </div>
+
+        {/* Caoursel Work End */}
+
         <div className="w-5/6 p-4 sm:p-8 bg-white m-auto my-5 rounded-2xl shadow-2xl flex flex-col md:flex-row  gap-6">
           <div className="flex flex-col gap-6 md:w-3/5">
             <div className="flex items-center h-[300px]">
